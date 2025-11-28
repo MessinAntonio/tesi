0e1988b7d73c00ca8e823730c58970ef050fde45
AntonioMessina <messinantonio13@gmail.com>
2025-11-28 11:28:04 +0100

    Add PrettyTable
    Aggiunti PrettyTable per analisi flash

--- scripts/to_store/mpmt_preprocess_flash.py
+++ scripts/to_store/mpmt_preprocess_flash.py
@@ -3,9 +3,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import argparse
 import os
-import json
-import time
-import math
+from prettytable import PrettyTable
+import numpy as np
 
 # ==== PARSING ARGOMENTI DA LINEA DI COMANDO ====
 parser = argparse.ArgumentParser(description = "Analisi ADC schede")
@@ -41,6 +40,7 @@ intervals = {
 fixed_range_x = args.fixed_range_x
 fixed_range_y = args.fixed_range_y
 dpi = 200
+flashEnable = True
 
 input_dir = "mpmt-board-cli_fileROOT"
 files = os.listdir(input_dir)
@@ -48,67 +48,98 @@ string = "Seleziona file da analizzare:\n"
 
 for i in range(len(files)):
     string += f" - Digita '{i}' per {files[i]}\n"
+string += f" - Digita 's' per fermare."
 print(string)
 
-idx_file = int(input())
-file_name = files[idx_file]
+while flashEnable:
+    idx_file = input("Digitare:")
 
-print(f"hai scelto il file {file_name}")
+    if idx_file == "s":
+        flashEnable = False
+        continue
 
-# ==== COSE PER ROOT ====
-input_file_path = os.path.join(input_dir, file_name)
-tree_name = "pmt_events"
-file = uproot.open(input_file_path)
-tree = file[tree_name]
+    idx_file = int(idx_file)
+    file_name = files[idx_file]
 
-ch = tree["channel"].array(library="np") + 1 
-adc = tree["adc"].array(library="np")
-df = pd.DataFrame({
-    "ch": ch,
-    "adc": adc
-})
+    print(f"hai scelto il file {file_name}")
+    # ==== COSE PER ROOT ====
+    input_file_path = os.path.join(input_dir, file_name)
+    tree_name = "pmt_events"
+    file = uproot.open(input_file_path)
+    tree = file[tree_name]
 
-print(df)
+    ch = tree["channel"].array(library="np") + 1 
+    adc = tree["adc"].array(library="np")
+    df = pd.DataFrame({
+        "ch": ch,
+        "adc": adc
+    })
 
-channels = range(1,20)
-for keys, values in intervals.items():
-    min_adc = values[0]
-    max_adc = values[1]
+    print(df)
 
-    # Preparazione griglia per subplot
-    fig, axes = plt.subplots(4, 5, figsize=(10, 6), sharex=fixed_range_x, sharey=fixed_range_y)
-    axes = axes.flatten()
-    
-    for idx, ch_id in enumerate(channels): 
-        printDebug(f"info: Analisi dei {keys} per canale {ch_id} di file '{os.path.basename(input_file_path)}'")
+    channels = range(1,20)
+    for keys, values in intervals.items():
 
-        bins = None
-        df_ch = df.loc[(df.ch == ch_id) & (min_adc < df.adc) & (df.adc < max_adc)] 
-        df_ch_mean = df_ch["adc"].mean()
-        ax = axes[idx]
+        # ==== COSTRUZIONE TABELLA RISULTATI ====
+        results_table = PrettyTable()
+        results_table.field_names = ["Segnale", "Canale", "Media", "Dev Std", "N eventi"]
 
-        # Se non ci sono dati
-        if df_ch.empty:
-            # subplot
-            ax.set_title(f"Ch{ch_id:02d} (EMPTY)")
+        min_adc = values[0]
+        max_adc = values[1]
+
+        # Preparazione griglia per subplot
+        fig, axes = plt.subplots(4, 5, figsize=(10, 6), sharex=fixed_range_x, sharey=fixed_range_y)
+        axes = axes.flatten()
+        
+        for idx, ch_id in enumerate(channels): 
+            printDebug(f"info: Analisi dei {keys} per canale {ch_id} di file '{os.path.basename(input_file_path)}'")
+
+            bins = None
+            df_ch = df.loc[(df.ch == ch_id) & (min_adc < df.adc) & (df.adc < max_adc)] 
+            df_ch_mean = df_ch["adc"].mean()
+            ax = axes[idx]
+
+            if df_ch.empty:
+                mean_val = "-"
+                std_val = "-"
+                count_val = 0
+            else:
+                mean_val = np.mean(df_ch["adc"])
+                std_val  = np.std(df_ch["adc"])
+                count_val = len(df_ch)
+
+            results_table.add_row([
+                keys,
+                f"{ch_id:02d}",
+                f"{mean_val:.2f}" if mean_val != "-" else "-",
+                f"{std_val:.2f}" if std_val != "-" else "-",
+                count_val
+            ])
+
+            # Se non ci sono dati
+            if df_ch.empty:
+                # subplot
+                ax.set_title(f"Ch{ch_id:02d} (EMPTY)")
+                ax.grid(True, linestyle="--", alpha=0.4)
+                continue
+
+            if fixed_range_x:
+                bins = range(min_adc, max_adc)
+            else:
+                hist_min = df_ch['adc'].min()
+                hist_max = df_ch['adc'].max() + 2
+                bins = range(hist_min, hist_max)
+
+            ax.hist(df_ch['adc'], bins, edgecolor='black', align='left')
+            ax.set_title(fr"Ch{ch_id:02d} ($\mu$ = {df_ch_mean:.2f})")
             ax.grid(True, linestyle="--", alpha=0.4)
-            continue
 
-        if fixed_range_x:
-            bins = range(min_adc, max_adc)
-        else:
-            hist_min = df_ch['adc'].min()
-            hist_max = df_ch['adc'].max() + 2
-            bins = range(hist_min, hist_max)
+        fig.suptitle(f"Histogram ADC BOARD '{keys}' — {file_name}", y=0.98)
+        plt.tight_layout(rect=[0, 0, 1, 0.96])
+        plt.show(block=False)
+        plt.pause(0.01)
+        # plt.close()
 
-        ax.hist(df_ch['adc'], bins, edgecolor='black', align='left')
-        ax.set_title(fr"Ch{ch_id:02d} ($\mu$ = {df_ch_mean:.2f})")
-        ax.grid(True, linestyle="--", alpha=0.4)
-
-    fig.suptitle(f"Histogram ADC BOARD '{keys}' — {file_name}", y=0.98)
-    plt.tight_layout(rect=[0, 0, 1, 0.96])
-    plt.show(block=False)
-    plt.pause(0.01)
-    # plt.close()
-
-input()
+        print("\n\n=== RISULTATI STATISTICI ---", keys.upper(), "===\n")
+        print(results_table)
+        print("\n")
